e204be3357a9feba1ea019da2d917349b6c7901d
shifoc <32818143+shifoc@users.noreply.github.com>
2024-04-14 23:56:07 +0300

    Add jwt-decode library for token decoding and authentication

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -1,4 +1,21 @@
+import { jwtDecode, JwtPayload } from 'jwt-decode';
+
 export function isAuthenticated() {
-    const isLogged = localStorage.getItem('user');
-    return isLogged;
+    // Retrieve the 'user' object from local storage.
+    const user = localStorage.getItem('user');
+    if (!user) return false;
+
+    // Retrieve the 'token' from the 'user' object.
+    const { token } = JSON.parse(user);
+    if (!token) return false;
+
+    // Decode the token and check if it has expired.
+    const decodedToken = jwtDecode<JwtPayload>(token);
+    let currentDate = new Date();
+
+    if (decodedToken.exp && decodedToken.exp * 1000 < currentDate.getTime()) {
+        return false;
+    } else {
+        return true;
+    }
 }
